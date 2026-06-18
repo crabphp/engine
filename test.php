bfb8e931a8f444c3c3d1eb25a2351883b36f63b3
bangnokia <bangnokia@gmail.com>
2026-06-19 03:11:18 +0700

    wip

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ use Crab\Engine\EventLoop\SelectLoop;
 use Crab\Engine\EventLoop\TcpConnection;
 use Crab\Engine\Net\SocketAddress;
 use Crab\Engine\Net\TcpServer;
+use Crab\Engine\Protocol\TextProtocol;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -12,6 +13,7 @@ $address = SocketAddress::parse('tcp://0.0.0.0:9000');
 
 $server = new TcpServer($loop, $address, function ($client, string $remote) use ($loop): void {
     $connection = new TcpConnection($loop, $client, $remote);
+    $connection->useProtocol(new TextProtocol());
 
     $connection->onMessage = function (TcpConnection $connection, string $data): void {
         $connection->send("echo: $data");
